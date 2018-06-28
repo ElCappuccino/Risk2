13e0c1a6e5b2506486b8c8d3b21c3697826e21c0
Valentin <v.giraud@lyon.ort.asso.fr>
2018-06-28 16:05:37 +0200

    PlayerRepo

---
+++ src/main/java/repository/PlayerRepository.java
@@ -0,0 +1,32 @@
+package repository;
+
+import java.util.List;
+
+import beans.PlayerBean;
+
+public interface PlayerRepository {
+    /**
+     * Permet de récupérer tous les enregistrements de PlayerBean
+     * @return Liste de Player
+     */
+	List<PlayerBean> getAll();
+	
+    /**
+     * Permet d'ajouter un nouvel enregistrement de PlayerBean
+     * @param player
+     */
+    void create(PlayerBean player);
+    
+    /**
+     * 
+     * Permet de supprimer un PlayerBean
+     * @param player
+     */
+    void delete(PlayerBean player);
+
+    /**
+     * Permet de mettre à jour un PlayerBean 
+     * @param player
+     */
+    void update(PlayerBean player);
+}
---
+++ src/main/java/repository/PlayerRepositoryImpl.java
@@ -0,0 +1,33 @@
+package repository;
+
+import java.util.List;
+
+import beans.PlayerBean;
+
+public class PlayerRepositoryImpl implements PlayerRepository{
+
+	@Override
+	public List<PlayerBean> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create(PlayerBean player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(PlayerBean player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(PlayerBean player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
